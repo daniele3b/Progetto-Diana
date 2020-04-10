c041e190dd77c23bf60e5371be57446bcbccacfe
Giotto-maker <luca.andolfi03@gmail.com>
2020-04-10 17:30:31 +0200

    Integration testing on Announcement routes + docs + jest and swagger configuration and code refactoring

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 
-
-
 const port = process.env.PORT || 8080
 
 require('./startup/db')()
 require('./startup/routes')(app)
 
-app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
+const server = app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
 
+module.exports = server
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const swaggerDocs = require('../startup/swagger')   // ADDED 
+const swaggerUi = require('swagger-ui-express')     // ADDED
 const announcements = require('../routes/announcements')
 
 module.exports = function(app) {
     app.use(express.json())
     app.use('/announcements', announcements)
+    app.use('/diana-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))   // ADDED
 }
