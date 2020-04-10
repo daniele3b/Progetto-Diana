21e0109d72d34e96621a695c5640169194083fea
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-10 21:09:17 +0200

    modified routes.js file for using the traffic route

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const announcements = require('../routes/announcements')
 const chemicalagent=require('../routes/chemicalagents')
+const traffic = require('../routes/traffic')
 
 module.exports = function(app) {
     app.use(express.json())
     app.use('/announcements', announcements)
     app.use('/chemical_agents',chemicalagent)
+    app.use('/traffic', traffic)
 }
