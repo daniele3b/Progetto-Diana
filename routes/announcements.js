2530ba33b2fd7d70ab633c3262ab520d380e9bdc
Giotto-maker <luca.andolfi03@gmail.com>
2020-04-10 10:48:26 +0200

    New feature to query announcements by date intervals + nodemon installed

--- routes/announcements.js
+++ routes/announcements.js
@@ -23,19 +23,21 @@ router.get('/', async (req,res) => {
     res.send(announcements)
 })
 
+
 router.get('/:CF', async (req,res) => {
     const announcements = await Announcement.find({CF: req.params.CF})
     if (!announcements) return res.status(404).send('No announcements match the given criteria')
     res.send(announcements)
 })
 
-/*  WORK IN PROGRESS TO QUERY DB ON A DATE INTERVAL...
-router.get('/:start,end', async (req,res) => {
-    const announcements = await Announcement.find({start: {'$gte': new Date(req.params.start)}})
+
+router.get('/:date_start/:date_end', async (req,res) => {
+    const date_start = new Date(req.params.date_start)
+    const date_stop = new Date(req.params.date_end)
+    const announcements = await Announcement.find({start: {'$gte': date_start, '$lt': date_stop}})
     if (!announcements) return res.status(404).send('No announcements match the given criteria')
     res.send(announcements)
 })
-*/
 
 
 module.exports = router
