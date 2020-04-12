d4e91266c003972aee65833c9aa17f067dac1fa9
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-12 09:27:01 +0200

    added winston + created logger to use to log error on logerror.log

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const winston=require('winston')
+const {logger}=require('./startup/logging')
 const app = express()
 const config=require('config')
 const {updateChemicalAgents}=require('./startup/updater')
 const {updateMeteo}=require('./startup/updater_meteo')
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080 
 
 require('./startup/db')()
 require('./startup/routes')(app)
 
 const server = app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
 
-
-setInterval(updateChemicalAgents,config.get('aqi_time_int'))
+setInterval(updateChemicalAgents,config.get('aqi_time_int')) //1 minute
 
 setInterval(updateMeteo, config.get('timer_meteo')) //15 seconds
 
