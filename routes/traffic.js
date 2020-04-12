65c1daa1785aa1fc60889b84a667ce21e89e5606
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-12 14:52:59 +0200

    added a new route for traffic route

--- routes/traffic.js
+++ routes/traffic.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const config = require('config')
 const request = require('request')
+const mongoose = require('mongoose')
+//const {ChemicalAgent} = require('../models/chemical_agents')
+const {Agents,Chemical_Agent,validate}=require('../models/chemical_agents')
 
 require('dotenv').config()
 
@@ -15,10 +18,10 @@ require('dotenv').config()
 
 /**
 * @swagger 
-* /traffic/address:
+* /traffic/:address:
 *  get:
 *    tags: [Traffic]
-*    description: Use to request all current data about traffic into an area specified by a given address(latest available)
+*    description: Used to request all current data about traffic into an area specified by a given address(latest available)
 *    responses:
 *       '200':
 *         description: A successful response, data available
@@ -85,5 +88,129 @@ router.get('/:address', (req, res) => {
         }
     })
 })
+
+/**
+* @swagger 
+* /traffic/:address/sensor:
+*  get:
+*    tags: [Traffic]
+*    description: Used to request the nearest sensor for a specified address(latest available)
+*    responses:
+*       '200':
+*         description: A successful response, data available
+*       '404':
+*         description: No data available
+*/
+
+router.get('/:address/sensor', (req, res) => {
+    
+    const address = req.params.address
+    const size = address.length
+    let parsed = ''
+    for(let i=0;i<size;i++){
+        if(address[i] == ' ') parsed += "%20"
+        else parsed += address[i]
+    }
+
+    const url_addr = pick_point_end_url+'/?key='+process.env.ADDRESS+'&q='+parsed+"&limit=1&format=json"
+
+    request(url_addr, async function(error, response, body) {
+        if(error) {
+            console.error('error:', error);
+            return
+        }
+
+        else {
+            const parsed_body = JSON.parse(body)
+            const lat = parsed_body[0].lat
+            const lon = parsed_body[0].lon
+            //console.log('lat='+lat+'lon='+lon)
+            
+
+            let result = await Chemical_Agent.find().select('sensor uid lat long -_id')
+            const dim = result.length
+            if(dim > 0) {
+                
+                let sensors = []
+                let uids = []
+                let coordinates = []
+
+                let i
+                for(i=0;i<dim;i++){
+                    if(!search(uids, result[i].uid)){
+                        uids.push(result[i].uid)
+                        sensors.push(result[i].sensor)
+                        coordinates.push({lat : result[i].lat, lon : result[i].long})
+                    }
+                }
+
+                //console.log(sensors)
+                //console.log(uids)
+                //console.log(coordinates)
+
+//https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=lat,lon8&destinations=DEST_LIST
+//&travelMode=driving&distanceUnit=km&key=process.env.DISTANCE
+                const bing_maps_end = config.get('bing_maps_end')
+                let bing_maps_end_url = bing_maps_end+'?origins='+lat+','+lon+'&destinations='
+
+                const size = coordinates.length
+                for(i=0;i<size;i++) {
+                    bing_maps_end_url += coordinates[i].lat+','+coordinates[i].lon
+                    if(i != size - 1) bing_maps_end_url += ';'
+                }
+                
+                bing_maps_end_url += '&travelMode=driving&distanceUnit=km&key='+process.env.DISTANCE
+
+                request(bing_maps_end_url, function(error, response, body) {
+                    if(error) {
+                        console.error(error)
+                        return
+                    }
+
+                    else {
+                        const parsed_body = JSON.parse(body)
+                        const results = parsed_body.resourceSets[0].resources[0].results
+                        const destinations = parsed_body.resourceSets[0].resources[0].destinations
+                        //console.log(results)
+                        
+                        const dim = results.length
+                        let dist_min = 999999
+                        let ind_min = -1
+                        for(i=0;i<dim;i++) {
+                            if(results[i].travelDistance < dist_min) {
+                                dist_min = results[i].travelDistance
+                                ind_min = results[i].destinationIndex
+                            }
+                        }
+                        res.status(200).send({
+                            sensor: sensors[ind_min],
+                            uid: uids[ind_min],
+                            coordinates: {
+                                lat: coordinates[ind_min].lat,
+                                lon: coordinates[ind_min].lon
+                            },
+                            distance: dist_min
+                        })
+                    }
+                })
+            }
+
+            else {
+                return res.status(400).send('No data available')
+            }
+        }
+    })
+
+})
+
+function search (arr, elem) {
+    let i
+    const dim = arr.length
+    for(i=0;i<dim;i++){
+        if(arr[i] == elem) return true
+    }
+    return false
+}
+
 
 module.exports = router
