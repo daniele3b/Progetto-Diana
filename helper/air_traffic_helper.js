1f0c6917002f0f577be30a4254b37269f956f2fd
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-14 18:34:05 +0200

    added helper for air_traffic_routes

---
+++ helper/air_traffic_helper.js
@@ -0,0 +1,11 @@
+const config = require('config')
+const request = require('request')
+
+require('dotenv').config()
+
+function checkDate(date) {
+    // WORK IN PROGRESS 
+    return true
+}
+
+exports.checkDate = checkDate
