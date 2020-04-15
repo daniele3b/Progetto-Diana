2d6612c4c448ae8df1973932c2bfce26e84383fb
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-15 12:51:50 +0200

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const winston=require('winston')
 const {logger}=require('./startup/logging')
 const app = express()
-
+var cors = require('cors')
 const config=require('config')
 const {updateChemicalAgents}=require('./startup/updater')
 const {updateMeteo}=require('./startup/updater_meteo')
@@ -11,6 +10,9 @@ const port = process.env.PORT || 8080
 
 require('./startup/db')()
 require('./startup/routes')(app)
+
+
+app.use(cors())
 
 const server = app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
 
