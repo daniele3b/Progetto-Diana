186741fc9c432d13ea58ea5a5e1658b55ffba0af
Giotto-maker <luca.andolfi03@gmail.com>
2020-04-15 20:51:12 +0200

    Added zone field in announcement model, routes updated and some other changes

--- helper/generic_helper.js
+++ helper/generic_helper.js
@@ -5,3 +5,16 @@ function validateDate(date)
 
 
 exports.validateDate=validateDate
+
+
+/*  TO BE USED AS MIDDLEWARE!
+*
+module.exports = function(req,res,next) {
+    if (req.params.date_start && ( isNaN(date_start) || date_start instanceof Date) ) 
+        return false
+    if (req.params.date_stop && ( isNaN(date_stop) || date_stop instanceof Date) )
+        return false 
+    return true
+}
+*
+*/
