474a1dab8275aafd965a3c5f15f774021ad3429c
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-17 12:05:20 +0200

    refactoring + added helper for testing

--- tests/unit/updater.test.js
+++ tests/unit/updater.test.js
@@ -3,13 +3,10 @@ const {updateChemicalAgents}=require('../../startup/updater')
 const mongoose=require('mongoose')
 const {Agents,Chemical_Agent,validate}=require('../../models/chemical_agents')
 const config=require('config')
+const {sleep}=require('../../helper/test_helper')
 
 jest.setTimeout(config.get('updater_to'))
 
-const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
-  
 
 let server
 
