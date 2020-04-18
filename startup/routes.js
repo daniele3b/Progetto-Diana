cc2ccbf2d8ed80ef25621a89143d5d58f95f1548
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-18 16:35:43 +0200

    nothing

--- startup/routes.js
+++ startup/routes.js
@@ -7,12 +7,9 @@ const chemicalagent=require('../routes/chemicalagents')
 const traffic = require('../routes/traffic')
 const air_traffic = require('../routes/air_traffic')
 const meteo = require('../routes/meteos')
-<<<<<<< HEAD
 const auth = require('../routes/auth')
-=======
 const registration = require('../routes/registration')
 const report = require('../routes/report')
->>>>>>> 492bbfac1cc4ff512135eb35de521134c8b6fd34
 
 module.exports = function(app) {
     app.use(express.json())
@@ -23,10 +20,7 @@ module.exports = function(app) {
     app.use('/traffic', traffic)
     app.use('/weather', meteo)
     app.use('/air_traffic', air_traffic)
-<<<<<<< HEAD
     app.use('/auth', auth)
-=======
     app.use('/registration',registration)
     app.use('/report', report)
->>>>>>> 492bbfac1cc4ff512135eb35de521134c8b6fd34
 }
