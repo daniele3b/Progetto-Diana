762ebef75ae4dc2f9d008b4cfcd2f88bbf5b26db
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-18 18:26:53 +0200

    nothing

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     },
     type: {
         type: String,
-        enum: ['cittadino', 'operatore'],
+        enum: ['cittadino', 'operatore','admin'],
         required: true
     },
     name: {
