479c621c7777d6d50a9909580c8bd7abccbdd7ac
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-18 18:01:58 +0200

    added route to change pw for citizien but i need token

--- routes/registration.js
+++ routes/registration.js
@@ -73,6 +73,55 @@ router.post('/citizen' , async (req,res) => {
 })
 
 
+
+
+
+/**
+* @swagger 
+* /registration/citizen/change_pw:
+*  post:
+*    tags: [Chemical_Agents]
+*    parameters:
+*       - name: User object
+*         description: object in JSON format with name,surname, birthdate(YYYY-MM-DD),birthplace,email or phone,password
+*         
+*    description: Use to change a pw of a citizien (type cittadino)
+*    responses:
+*       '200':
+*         description: A successful request
+*                                  
+*       '404':
+*         description: No data available
+*/
+
+router.post('/citizen/change_pw' , async (req,res) => {
+
+        if(req.body.new_pw ==undefined || req.body.old_pw==undefined)
+         return res.status(400).send('Bad request')
+        let cf='' //DA PRENDERE DAL TOKEN
+
+        let result=await User.find({CF:cf})
+        if(result.length==0) {
+            res.status(404).send('User not found')
+        }
+        else{
+            if(result.type!='citizien'){
+                return res.status(400).send('Bad request 2')
+            }else
+            {
+                const validPassword = await bcrypt.compare(req.body.old_password, user.password);
+                if(!validPassword) 
+                    return res.status(400).send('Invalid password');
+                else{
+                    const salt = await bcrypt.genSalt(config.get('pw_salt'));
+                    const np=await bcrypt.hash(req.body.new_pw, salt);
+                    const user= await User.findOneAndUpdate({CF:cf},{password:np})
+                    return res.status(200).send('Password updated')
+                }
+            }
+        }
+    
+    })
 /**
 * @swagger 
 * /registration/operator:
