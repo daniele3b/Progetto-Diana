f40827f88cb7e0252d75d40a596d5dfa32b821d2
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-18 21:34:38 +0200

    added other tests

--- tests/integration/registration.test.js
+++ tests/integration/registration.test.js
@@ -5,9 +5,7 @@ const {User,validateUser}=require('../../models/user')
 const {calculateCF}=require('../../helper/registration_helper')
 const bcrypt=require('bcrypt')
 
-
 let server
-
 let name
 let surname
 let birthdate
@@ -153,6 +151,64 @@ describe('/registration', () => {
                 .set('x-diana-auth-token', citizen_token);
 
             expect(res.status).toBe(200);
+        });
+
+
+
+        it('should return 404 if user doesnt exist in and all field is setted', async () => {
+          
+            const res = await request(server)
+                .post('/registration/citizen/change_pw')
+                .send({old_pw:'aCertainPassword!',new_pw:'newPassword!'})
+                .set('x-diana-auth-token', citizen_token);
+
+            expect(res.status).toBe(404);
+        });
+
+
+        
+        it('should return 400 if user exist but he isnt a cittadino', async () => {
+            const salt = await bcrypt.genSalt(config.get('pw_salt'));
+           
+            let us=new User({CF:cf_citizen,name : 'Daniele',surname : 'Bufalieri',type:'operatore',sex: 'M',birthdate : '1998-12-02', birthplace : 'Roma', email : 'federeristheway@gmail.com',phone : '1234567890',password : 'aCertainPassword!'})
+            us.password= await bcrypt.hash(us.password, salt);
+            await us.save()
+            const res = await request(server)
+                .post('/registration/citizen/change_pw')
+                .send({old_pw:'aCertainPassword!',new_pw:'newPassword!'})
+                .set('x-diana-auth-token', citizen_token);
+
+            expect(res.status).toBe(400);
+        });
+
+
+        it('should return 400 if user exist but old pw is wrong', async () => {
+            const salt = await bcrypt.genSalt(config.get('pw_salt'));
+           
+            let us=new User({CF:cf_citizen,name : 'Daniele',surname : 'Bufalieri',type:'cittadino',sex: 'M',birthdate : '1998-12-02', birthplace : 'Roma', email : 'federeristheway@gmail.com',phone : '1234567890',password : 'aCertainPassword!'})
+            us.password= await bcrypt.hash(us.password, salt);
+            await us.save()
+            const res = await request(server)
+                .post('/registration/citizen/change_pw')
+                .send({old_pw:'error',new_pw:'newPassword!'})
+                .set('x-diana-auth-token', citizen_token);
+
+            expect(res.status).toBe(400);
+        });
+
+
+        
+        it('should return 400 if user exist but old pw and new pw arent setted', async () => {
+            const salt = await bcrypt.genSalt(config.get('pw_salt'));
+           
+            let us=new User({CF:cf_citizen,name : 'Daniele',surname : 'Bufalieri',type:'cittadino',sex: 'M',birthdate : '1998-12-02', birthplace : 'Roma', email : 'federeristheway@gmail.com',phone : '1234567890',password : 'aCertainPassword!'})
+            us.password= await bcrypt.hash(us.password, salt);
+            await us.save()
+            const res = await request(server)
+                .post('/registration/citizen/change_pw')
+                .set('x-diana-auth-token', citizen_token);
+
+            expect(res.status).toBe(400);
         });
 
 
