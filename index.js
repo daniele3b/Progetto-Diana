4f6324352215d7324edc1cce92a9d34f7e38403d
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-04-18 14:29:40 +0200

    added superuser startup + other things

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var cors = require('cors')
 const config=require('config')
 const {updateChemicalAgents}=require('./startup/updater')
 const {updateMeteo}=require('./startup/updater_meteo')
+const {SuperUser_startup}=require('./startup/superuser_startup')
+
 
 const port = process.env.PORT || 8080 
 
@@ -16,6 +18,13 @@ app.use(cors())
 
 const server = app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
 
+SuperUser_startup()
+.then((result)=>{if(res==1)
+    console.log('Admin setted') 
+    else console.log('Admin already exist')
+})
+
+.catch((error)=>{console.log(error)})
 if(process.env.NODE_ENV!="test"){
 setInterval(updateChemicalAgents,config.get('aqi_time_int')) //1 minute
 
