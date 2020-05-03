7d7e1b37f08d85f4ac2dde7b83c6c59ce457d9a9
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-05-03 09:38:52 +0200

    aggiusto meteo helper

--- startup/updater_meteo.js
+++ startup/updater_meteo.js
@@ -13,20 +13,7 @@ function updateMeteo(){
 
             var d = new Date(info.dt*1000);
             var data = d.toISOString()
-            /*
-            var day = d.getDate().toString(); 
-            if(day.length==1) day = '0'+day
-            var month = (d.getMonth()+1).toString();
-            if(month.length==1) month = '0'+month
-            var data = day + "/" + month + "/" + d.getFullYear();
             
-            var hh = d.getHours().toString();
-            if(hh.length==1) hh = '0'+hh
-            var mm = d.getMinutes().toString();
-            if(mm.length==1) mm = '0'+mm
-            var ss = d.getSeconds().toString();
-            if(ss.length==1) ss = '0'+ss
-            var orario =  hh + ":" + mm + ":" + ss;*/
 
             var datastamp = info.dt;
             var descrizione = info.weather[0].main + ", " + info.weather[0].description;
@@ -48,6 +35,7 @@ function updateMeteo(){
             try{
                 let res=await Meteo.findOneAndDelete({datastamp: datastamp})
                 const result = await meteo.save();
+                console.log("aggiunto "+meteo)
             }catch(ex){
                 console.error('error:', error);
                 logger.error('M2: Impossibile to save meteo report to databse')
