bae3cabdc1d16b446a7b7b9f86dbf31fcf0d79d6
Giotto <luca.andolfi03@gmail.com>
2020-05-29 21:11:57 +0200

    google-auth doc

--- routes/google-auth.js
+++ routes/google-auth.js
@@ -2,12 +2,24 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
-
+/**
+* @swagger
+* /sign-in/google:
+*  get:
+*    tags: [OAuth 2.0]
+*    description: Use to register and login using your Google account.
+*/
 router.get('/', passport.authenticate('google', {
     scope: ['email','profile']
 }))
 
-
+/**
+* @swagger
+* /sign-in/google/redirect:
+*  get:
+*    tags: [OAuth 2.0]
+*    description: Redirect URL hitten after a successfull login with your Google account.
+*/
 router.get('/redirect', passport.authenticate('google'), (req,res) => {
     const user = {
         gid: req.user.googleId,
